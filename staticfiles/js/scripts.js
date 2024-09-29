1f60f8c3976841d7e2aa19171346736dabccd001
harshadakhorgade <harshadakhorgade9@gmail.com>
2024-09-29 19:35:44 +0530

    Update .gitignore to include nginx, gunicorn, and scripts directories

---
+++ staticfiles/js/scripts.js
@@ -0,0 +1,9 @@
+function showNextSection() {
+    document.getElementById('form-section').style.display = 'none';
+    document.getElementById('info-section').style.display = 'block';
+}
+
+function goToApp() {
+    // Add logic to redirect to the app or desired page
+    alert("Redirecting to the app...");
+}
