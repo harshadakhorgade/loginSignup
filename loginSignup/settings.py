5b706249f32ad9067eec02a5a9e7dab50bfc5bf3
harshadakhorgade <harshadakhorgade9@gmail.com>
2024-09-29 21:27:14 +0530

    Update urls

--- loginSignup/settings.py
+++ loginSignup/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic', #Make sure to add this 
     'base'
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #make sure to add this line
 ]
 
 ROOT_URLCONF = 'loginSignup.urls'
